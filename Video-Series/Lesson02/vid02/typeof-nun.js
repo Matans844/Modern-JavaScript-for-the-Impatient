2ab7882102d1a9493dfb94009a07b333be995780
Lord-Whitesmith <matans844@gmail.com>
2023-04-04 18:50:28 +0300

    Started Lesson 02: Control Flow Main conclusion: Avoid automatic conversions / coercions. These can happen with any operator. To do this, always specify operand type explicitly before using an operator, so that all operands going in to the operator will have the same type!

---
+++ Video-Series/Lesson02/vid02/typeof-nun.js
@@ -0,0 +1,7 @@
+console.log(typeof NuN)
+// This returns undefined, because NuN is a variable reference, which is an expression.
+// That said, there was no NuN variable defined.
+// What I was looking for here is the following
+
+console.log(typeof  NaN)
+// This return number
